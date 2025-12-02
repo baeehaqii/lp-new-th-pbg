9c8d5917bd9e251398edb99dea0ea0c1be7056ea
Unname <muhamadimambaehaqi@gmail.com>
2025-12-02 16:40:19 +0700

    update gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,13 @@ export default function RootLayout({
         </Script>
 
         {/* Google Tag Manager */}
-
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M88WJ5KJ');`
+        }} />
         {/* End Google Tag Manager */}
 
         {/* Facebook Pixel Code */}
@@ -92,7 +98,9 @@ export default function RootLayout({
       </head>
       <body style={{ fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif' }}>
         {/* Google Tag Manager (noscript) */}
-
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M88WJ5KJ" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
         {/* End Google Tag Manager (noscript) */}
 
         <Suspense fallback={null}>
